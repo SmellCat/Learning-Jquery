0ab4bd6ae0fa759ca4de146de28186d4e67adcad
Atituset <1433174900@qq.com>
2019-08-09 22:22:08 +0800

    第二章完成

--- Chapter_2/complete.js
+++ Chapter_2/complete.js
@@ -16,5 +16,32 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     // $('tr:even').addClass('alt'); // eq()、:odd、:even选择符都使用JavaScript内置从0开始编号
-    $('tr:nth-child(odd)').addClass('alt');
+    $('tr:nth-child(odd)').addClass('alt'); // :nth-child()是JQuery中唯一从1开始计数的选择符。
+    // $('tr').filter(':even').addClass('alt');
+    // $('td:contains(Henry)').addClass('highlight');
+    // $('td:contains(Henry)').next().addClass('highlight');
+    // $('td:contains(Henry)').nextAll().addClass('highlight');
+    // $('td:contains(History)').prev().addClass('highlight');
+    // $('td:contains(History)').prevAll().addClass('highlight');
+    // $('td:contains(Henry)').siblings().addClass('highlight');
+    // $('td:contains(History)').siblings().addClass('highlight');
+    // $('td:contains(Henry)').nextAll().addBack().addClass('highlight');
+    // $('td:contains(Henry)').parent().children().addClass('highlight');
+});
+
+$(document).ready(function () {
+    $('a').filter(function() {
+        return this.hostname && this.hostname != location.hostname;
+    }).addClass('external');
+});
+
+$(document).ready(function() {
+    // 连缀
+   $('td:contains(Henry)') // 取得包含Henry的所有单元格
+       . parent() // 取得它的父元素
+       .find('td:eq(1)') // 在父元素中查找第2个单元格
+       .addClass('highlight') // 为该单元格添加highlight类
+       .end()  // 恢复到包含Henry的单元格的父元素
+       .find('td:eq(2)') // 在父元素中查找第3个单元格
+       .addClass('highlight') // 为该单元格添加highlight类
 });
